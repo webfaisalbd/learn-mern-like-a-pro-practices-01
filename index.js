7e80eaeb40845da64aea65c2f89c9c24860a3ad4
webfaisalbd <web.faisal.bd@gmail.com>
2022-07-21 13:32:59 +0600

    sum of array, using split, map

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 // Write my code
 
-const strNumbers = prompt('Provide Input the numbers As an Array:');
-const numbers = JSON.parse(strNumbers);
+const strNumbers = prompt('Provide Input the numbers As Comma by Comma:');
+const splitNumber = strNumbers.split(',');
+console.log(splitNumber); // we get array but string of array like ['1','2','3']
+
+// make (string of array) to (number of array)
+const numberOfArray = splitNumber.map((x) => {
+  return parseInt(x);
+});
+console.log(numberOfArray); // we get number array like [1,2,3]
+
 let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-  const n = numbers[i];
+for (let i = 0; i < numberOfArray.length; i++) {
+  const n = numberOfArray[i];
   sum = n + sum;
 }
 console.log('Total: ', sum);
