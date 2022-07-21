60b5963055bc74a21131325cab2feace38e2489a
webfaisalbd <web.faisal.bd@gmail.com>
2022-07-21 13:26:06 +0600

    sum of array, without user input

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 // Write my code
+
+const numbers = [1, 2, 3, 4];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  const n = numbers[i];
+  sum = n + sum;
+}
+console.log('Total: ', sum);
